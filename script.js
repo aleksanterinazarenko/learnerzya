66891b77fa8f67fb997d73b34d50b3506b82c0c4
Aleksanteri Nazarenko <aleksanterinazarenko@gmail.com>
2024-08-22 11:15:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("DOM fully loaded and parsed");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-list a');
+  const currentPath = window.location.pathname.split("/").pop();
+
+  navLinks.forEach(link => {
+      const linkPath = link.getAttribute('href');
+      if (linkPath === currentPath) {
+          link.classList.add('active');
+      }
+  });
+});
